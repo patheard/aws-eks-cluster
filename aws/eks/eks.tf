b202da4a0cb272375c955c6a96d046a1f1a722ce
Pat Heard <patrick.heard@cds-snc.ca>
2022-04-17 19:38:08 +0000

    fix: kube-proxy & coredns version for k8s 1.19

--- aws/eks/eks.tf
+++ aws/eks/eks.tf
@@ -71,13 +71,13 @@ resource "aws_eks_addon" "vpc_cni" {
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name      = aws_eks_cluster.test_cluster.name
   addon_name        = "kube-proxy"
-  addon_version     = "v1.20.7-eksbuild.1"
+  addon_version     = "v1.19.6-eksbuild.2"
   resolve_conflicts = "OVERWRITE"
 }
 
 resource "aws_eks_addon" "coredns" {
   cluster_name      = aws_eks_cluster.test_cluster.name
   addon_name        = "coredns"
-  addon_version     = "v1.8.3-eksbuild.1"
+  addon_version     = "v1.8.0-eksbuild.1"
   resolve_conflicts = "OVERWRITE"
 }
